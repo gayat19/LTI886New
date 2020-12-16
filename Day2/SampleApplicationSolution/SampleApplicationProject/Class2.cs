a91e9fe0fa6b143471604b952840f99be53dabd5
Gayathri Mahadevan <gayat_19@yahoo.com>
2020-12-16 16:02:25 +0530

    Spl Day2

---
+++ Day2/SampleApplicationSolution/SampleApplicationProject/Class2.cs
@@ -0,0 +1,11 @@
+using System;
+namespace SampleApplicationProject
+{
+    partial class Program
+    {
+        void PrintWelcome()
+        {
+            Console.WriteLine("Welcome by programmer 2");
+        }
+    }
+}
---
+++ Day2/SampleApplicationSolution/SampleApplicationProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplicationProject
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().PrintWelcome();
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Another line");
+            Console.ReadKey();
+        }
+    }
+}
